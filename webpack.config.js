588075b3c655e861d2bfeed2ff0ba5f7eaa14963
Vurnex <hamilton.laquon@gmail.com>
2023-06-11 19:35:53 -0500

    Configure webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -6,10 +7,25 @@ module.exports = {
     devServer: {
         static: './dist',
     },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Restaurant',
+        }),
+      ],
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
     optimization: {
         runtimeChunk: 'single',
     },
