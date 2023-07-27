656d7335a15edc24644215bca30c850dba7d9167
Vurnex <hamilton.laquon@gmail.com>
2023-07-26 20:57:23 -0500

    Add about section to home tab

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,10 @@
 import pizza_intro from "../img/pizza-intro.jpg";
+import group_img from "../img/group.jpg";
 
 export default function home() {
 
+    //Append all content to this div
+    const content = document.createElement('div');
 
     //Intro
     const intro = document.createElement('section');
@@ -32,10 +35,10 @@ export default function home() {
 
     intro.appendChild(intro_left);
 
+
     const intro_right = document.createElement('div');
     intro_right.setAttribute('class', 'intro-right');
 
-    
     const intro_img = document.createElement('img');
     intro_img.setAttribute('src', pizza_intro);
     intro_img.setAttribute('width', '500px');
@@ -44,7 +47,53 @@ export default function home() {
 
     intro.appendChild(intro_right);
 
-    return intro;
+    //About
+
+    const about = document.createElement('section');
+    about.setAttribute('class', 'about');
+
+    const about_left = document.createElement('div');
+    about_left.setAttribute('class', 'about-left');
+
+    const about_h6 = document.createElement('h6');
+    const about_h1 = document.createElement('h1');
+    const about_para = document.createElement('p');
+
+    about_h6.textContent = "Blog";
+    about_h1.textContent = "THE NEW SPOT FOR PIZZA ENJOYERS";
+    about_para.textContent = 
+
+    `
+    We pack a large selection of pre-made delicious dish layouts 
+    as well as diverse toppings. Share all about your latest 
+    promotions and menu changes.
+    
+    `;
+
+    about_left.appendChild(about_h6);
+    about_left.appendChild(about_h1);
+    about_left.appendChild(about_para);
+    about_left.appendChild(button('View More'));
+
+    about.appendChild(about_left);
+
+    const about_right = document.createElement('div');
+    about_right.setAttribute('class', 'about-right');
+
+    const about_img = document.createElement('img');
+    about_img.setAttribute('src', group_img);
+    about_img.setAttribute('width', '500px');
+
+    about_right.appendChild(about_img);
+
+    about.appendChild(about_right);
+
+
+    //Append content
+    content.appendChild(intro);
+    content.appendChild(about);
+
+    return content;
     
 }
 
